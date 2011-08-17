53d69d885f572d623cbad5fa2740d7153f16b537
t.ashula <dev@ashula.info>
2011-08-17 16:15:41 +0900

    initial commit

---
+++ lib/Canonical.pm
@@ -0,0 +1,32 @@
+package Canonical;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Canonical -
+
+=head1 SYNOPSIS
+
+  use Canonical;
+
+=head1 DESCRIPTION
+
+Canonical is
+
+=head1 AUTHOR
+
+t.ashula E<lt>office@ashula.infoE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
